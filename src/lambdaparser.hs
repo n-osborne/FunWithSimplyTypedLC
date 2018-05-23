99a018426f7e8d96a51e83cb7b44909a92b4b841
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-05-23 22:19:36 +0200

    preparePg implementation
    really bad complexity, work to be done on that

--- src/lambdaparser.hs
+++ src/lambdaparser.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_HADDOCK ignore-exports #-}
 {-|
 Module : LambdaParser
-Copyrigth : Nicolas Osborne, 2018
+Copyrigth : (c) Nicolas Osborne, 2018
 Licence : MIT
 
 Provide the function pg2ast which turn a program written in Simply Typed Lambda
@@ -24,7 +24,7 @@ A program in Simply Typed Lambda Calculus is divided into two main parts:
 
 
 module LambdaParser
-  ( pg2ast ) where
+  ( ) where
 
 
 
@@ -40,11 +40,10 @@ data AST = Empty |
                 , name :: [Char] -- ^ Either a value or an id
                 } deriving (Show)
 
-
 -- | Turn a source file into the corresponding AST.
 --
 -- A source file must contain two main parts deliminated by the three following
---key words in that order: @Assignations@ @Program@ @End@. The assignation part
+--key words in that order: @Assignations@, @Program@, @End@. The assignation part
 --(between the two first key words) is a series of assignation instruction (with
 --the @let@ key word) and the program part (between the two lasts key words) is
 --the actual computation of which the source file is a description. The program
@@ -54,11 +53,7 @@ data AST = Empty |
 --
 -- [@output@]: the corresponding @AST@.
 pg2ast :: [Char] -> AST
-pg2ast pg = createAST list_tokens_pg_with_subst
-  where list_tokens_pg_with_subst = preparePg list_assignations list_tokens_pg
-        list_assignations = parseA extractAssignations pg
-        list_tokens_pg = selectProgram pg
-
+pg2ast pg = Empty
 
 
 -- Helper functions
@@ -119,8 +114,8 @@ extractPg (s:ns)
 -- [@Input@]: The list of tokens corresponding to the Assignation part of the
 --source file.
 --
--- [@Output@]: A list of Product Id x Exp
-
+-- [@Output@]: A list of assignations. The @head@ of each item is an @id@ and
+--the @tail@ is the @expression@
 parseA :: [[Char]] -> [[[Char]]]
 parseA tokens = oneSplit "let" tokens
   where oneSplit str [] = []
@@ -134,97 +129,21 @@ parseA tokens = oneSplit "let" tokens
                   | otherwise = s:read str ns
 
 
-
 -- | Substitute the Id in the list of tokens corresponding to the program part
 -- of the source file by the corresponding list of tokens.
 preparePg :: [[[Char]]] -> [[Char]] -> [[Char]]
-
-
+preparePg _ [] = []
+preparePg list1 (x:xs) = (check list1 x) ++ (preparePg list1 xs)
+  where check :: [[[Char]]] -> [Char] -> [[Char]]
+        check [] x = [x]
+        check (y:ys) x
+          | x == head y = tail y
+          | otherwise = check ys x
+         
 
 -- | Create the AST corresponding to the given list of tokens.
-createAST [[Char]] -> AST
+createAST :: [[Char]] -> AST
+createAST list = Empty
 
 
 
-
-
-
-{--
-
-import GHC.Unicode
-
--- | Abstract Syntax Tree type declaration.
---
--- An Ast is either an empty tree (correspond to an empty program), or a Node
--- with two children which are Abstract Syntax Trees with leaves.
--- Nodes are for lambda application, so they have an arity of two.
--- Leaves are for names.
-data Ast = Empty |
-           Node { len :: Int -- ^ length of the tree
-                , operator :: [Char] -- ^ tag for the operator, either App or Abs
-                , left :: Ast -- ^ either a binder or a lambda abstraction
-                , rigth :: Ast -- ^ a lambda expression
-                } |
-           Leaf { len :: Int -- ^ length of the leaf, which is obviously 1
-                , name :: [Char] -- ^ name: id:Type
-                } deriving (Show)
-
--- | Turn a program into the corresponding Abstract Syntax Tree.
-pg2ast :: [Char] -> Ast
-pg2ast pg = createAst (parsePg pg)
-
--- | Turn a program into the corresponding list of its tokens.
-parsePg :: [Char] -> [[Char]]
-parsePg [] = []
-parsePg (c:cs)
-  | c == '/' = "Abs" : parsePg cs
-  | c == '(' = "App" : parsePg cs
-  | c == ')' = parsePg cs
-  | c == '.' = parsePg cs
-  | c == ' ' = parsePg cs
-  | otherwise = name : parsePg (drop (length name) (c:cs))
-  where name = readName (c:cs)
-
-
--- | Turn a list of the tokens of a program into the corresponding Abstact
--- Syntax Tree.
-createAst :: [[Char]] -> Ast
-createAst [] = Empty
-createAst (c:cs)
-  | c == "Abs" = Node { len=n, operator=c, left=lAst, rigth=rAst }
-  | c == "App" = Node { len=n, operator=c, left=lAst, rigth=rAst }
-  | otherwise = Leaf { len=1, name=c }
-  where lAst = createAst cs
-        rAst = createAst (drop (len lAst) cs)
-        n = 1 + len lAst + len rAst
-
--- | Extract the string corresponding of a name from the point of the program we
--- have reached in the parsing.
-readName :: [Char] -> [Char]
-readName [] = []
-readName s
-  | length s >= 3 = readId s
-  | otherwise = "Error"
-
--- | Check that the id is lower case.
-readId :: [Char] -> [Char]
-readId (c:cs)
-  | (isLower c) == True = c:readTypeJgt cs
-  | otherwise = "Error"
-
--- | Check there is a colon between id and type.
-readTypeJgt :: [Char] -> [Char]
-readTypeJgt (c:cs)
-  | c == ':' = c:readType cs
-  | otherwise = "Error"
-
--- | Check the type is a known type.
-readType :: [Char] -> [Char]
-readType s
-  | take 4 s == "Bool" = "Bool"
-  | take 3 s == "Nat" = "Nat"
-  | take 3 s == "Str" = "Str"
-  | take 4 s == "Pair" = "Pair"
-  | otherwise = "Error"
-
---}
