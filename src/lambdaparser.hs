75a87297b720adc4db86cf003d0aabba2328079d
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-05-26 23:05:51 +0200

    lambdaparser completed

--- src/lambdaparser.hs
+++ src/lambdaparser.hs
@@ -22,32 +22,36 @@ A program in Simply Typed Lambda Calculus is divided into two main parts:
   previously defined in the @Assignations@ part.
 -}
 module LambdaParser
-  ( pg2ast ) where
+  ( file2ast
+  , exp2ast
+  ) where
 
--- Type declaration and main program:
+-- * Type declaration:
 
 -- | Abstract Syntax Tree type declaration.
 data AST =
   -- | Correspond to the constant of the language. 
-  Leave { name :: String -- ^ name of the constant
-           , ltype :: String -- ^ type of the constant
-           , size :: Int -- ^ size of the AST (here 1)
-           } |
+  Leave { name :: String
+        , ltype :: String
+        , size :: Int -- ^ size of the 'AST'
+        } |
   -- | Correspond to the unary operators of the language.
-  Unary { name :: String -- ^ name of the operator
-        , ltype :: String -- ^ type of the operator
-        , size :: Int -- ^ size of the AST (1 + size below)
+  Unary { name :: String
+        , ltype :: String
+        , size :: Int
         , below :: AST -- ^ argument of the operator
         } |
   -- | Correspond to the binary operators of the language.
-  Binary { name :: String -- ^ name of the operator
-         , ltype :: String -- ^ type of the operator
-         , size :: Int -- ^ size of the AST (1 + size left + size rigth)
-         , left :: AST -- ^ first argument of the operator
-         , right :: AST -- ^second argument of the operator
+  Binary { name :: String
+         , ltype :: String
+         , size :: Int
+         , left :: AST -- ^ first argument of the binary operator
+         , right :: AST -- ^second argument of the binary operator
          } deriving (Show)
 
--- | Turn a source file into the corresponding AST.
+-- * Main programs:
+
+-- | Turn a source file into the corresponding 'AST'.
 --
 -- A source file must contain two main parts deliminated by the three following
 --key words in that order: @Assignations@, @Program@, @End@. The assignation part
@@ -57,21 +61,27 @@ data AST =
 --part may contain the identificators declared in the assignation part.
 --
 -- [@input@]: The content of the source file.
--- [@output@]: the corresponding @AST@.
-pg2ast :: [Char] -> AST
-pg2ast pg = Leave "true" "bool" 1
+-- [@output@]: the corresponding 'AST' type checked.
+file2ast :: [Char] -> AST
+file2ast file = createAST (file2pg file)
 
+-- | Return the corresponding 'AST' of the given expression.
+--
+-- [@input@]: A well formed expression without any identificator, a "all in one"
+-- lambda expression.
+-- [@output@]: The corresponding 'AST' type checked.
+exp2ast :: [Char] -> AST
+exp2ast exp = createAST (words exp)
 
+-- * Build the Abstract Syntax Tree:
 
-
--- | Create the AST corresponding to the given list of tokens.
+-- | Create the 'AST' corresponding to the given list of tokens.
 --
 -- This function defines the syntax of the language.
 --
--- [@input@]: the list of tokens representing the program
--- [@output@]: the corresponding AST type checked
+-- [@input@]: the list of tokens representing the program.
+-- [@output@]: the corresponding 'AST' type checked.
 createAST :: [[Char]] -> AST
--- createAST list = Leave "true" "bool" 1
 createAST (s:ns)
   | elem s ["lambda","apply","pi"] = let l = createAST ns
                                          r = createAST (drop 1 ns)
@@ -93,40 +103,50 @@ createAST (s:ns)
   | s == "false" = Leave "false" "bool" 1
   | s == "null" = Leave "null" "empty" 1
   | length s == 1 = Leave s "char" 1
-  --  | otherwise =
+  | elem ':' s = let splitedVar = (oneSplit ':' s)
+                     n = fst splitedVar
+                     t = snd splitedVar
+                 in Leave n t 1
+  | otherwise = Leave "Error" "Error" 1
+         
+-- * Type manipulations:
+
+-- | Given two types and a binary operator, compute the expression's type.
+--
+-- [@input@]: operator name
+-- [@input@]: type of the first argument
+-- [@input@]: type of the second argument
+-- [@output@]: type of the expression
+computeBinaryType :: [Char] -> [Char] -> [Char] -> [Char]
+computeBinaryType "lambda" t1 t2 = "(" ++ t1 ++ ">" ++ t2 ++ ")"
+computeBinaryType "apply" t1 t2
+  | elem '>' t1 && (head subtypes) == t2 = head (drop 1 subtypes)
+  | otherwise = "type error"
+  where subtypes = splitType t1
+computeBinaryType "pi" t1 t2 = "(" ++ t1 ++ "x" ++ t2 ++ ")"
 
 -- | Compute type of a pair elimination
+--
+-- [@input@]: operator, either @first@ or @second@
+-- [@input@]: type of the pair, /ie/ of the from @AxB@
+-- [@output@]: type of the expression
 computePairElim :: [Char] -> [Char] -> [Char]
 computePairElim op pairType
   | op == "first" = head (splitType pairType)
   | op == "second" = head (drop 1 (splitType pairType))
   | otherwise = "Type Error"
 
--- | Given two types and a binary operator, compute the expresion's type.
-computeBinaryType :: [Char] -> [Char] -> [Char] -> [Char]
-computeBinaryType "lambda" t1 t2 = t1 ++ ">" ++ "t2"
-computeBinaryType "apply" t1 t2
-  | elem '>' t1 && (head subtypes) == t2 = head (drop 1 subtypes)
-  | otherwise = "type error"
-  where subtypes = splitType t1
-computeBinaryType "pi" t1 t2 = t1 ++ "x" ++ t2
+-- * Prepare the program into a list of tokens:
 
--- | Split a composed type in its two main parts.
-splitType :: [Char] -> [[Char]]
-splitType [] = []
-splitType t = splitType' 0 [] t
-  where 
-    splitType' :: Int -> [Char] -> [Char] -> [[Char]]
-    splitType' 0 t1 [] = [t1]
---    splitType' _ [] _ = ["Type Error"]
-    splitType' _ t1 [] = ["Type Error"]
-    splitType' n t1 (s:ns)
-      | n == 0 && (elem s ">x") = [t1, ns]
-      | s == '(' = splitType' (n+1) (t1 ++ [s]) ns
-      | s == ')' = splitType' (n-1) (t1 ++ [s]) ns
-      | otherwise = splitType' n (t1 ++ [s]) ns
-               
-
+-- | Prepare the content of a file for 'createAST' function.
+--
+-- [@input@]: The content of a file as described in the specifications.
+-- [@output@]: A list of tokens ready for building an AST.
+file2pg :: [Char] -> [[Char]]
+file2pg file = let listTokens = file2list file
+                   partA = extractAssignations listTokens
+                   partPg = extractPg listTokens
+               in preparePg (parseA partA) partPg
 
 -- TODO avoid use of length by computing length res when building res
 -- in order to do that: read :: x -> y -> [] -> (Int, [])
@@ -150,11 +170,6 @@ file2list pg = mydbsplit ' ' '\n' pg
               | c == y = []
               | otherwise = c:read x y cs
 
-{--
-would be version with import Data.List.Split
-file2list pg = split (condens . dropDelims oneOf "\n ") pg
--}
-  
 -- TODO handle commentary section before Assignations
 -- | Return the sub-list corresponding to the list of tokens corresponding to
 -- the Assignation part of the source file.
@@ -180,9 +195,9 @@ extractPg (s:ns)
 -- | Parse the Assignation part of the source file.
 --
 -- [@Input@]: The list of tokens corresponding to the Assignation part of the
---source file.
+-- source file.
 -- [@Output@]: A list of assignations. The @head@ of each item is an @id@ and
---the @tail@ is the @expression@
+-- the @tail@ is the @expression@
 parseA :: [[Char]] -> [[[Char]]]
 parseA tokens = oneSplit "let" tokens
   where oneSplit str [] = []
@@ -206,4 +221,42 @@ preparePg list1 (x:xs) = (check list1 x) ++ (preparePg list1 xs)
           | x == head y = tail y
           | otherwise = check ys x
          
+-- * Utils:
 
+-- | Split a string into two substrings, the prefix up to the first occurrence
+-- of the given Char, and the suffix beginning after this occurrence.
+--
+-- [@input@]: the separator
+-- [@input@]: the string
+-- [@output@]: the pair of the two substrings
+oneSplit :: Char -> [Char] -> ([Char], [Char])
+oneSplit c str = (fst res, drop (1+(snd res)) str)
+  where res = substring 0 c [] str
+        substring :: Int -> Char -> [Char] -> [Char] -> ([Char], Int)
+        substring n c w [] = (w, n)
+        substring n sep w (c:cs)
+          | c == sep = (w, n)
+          | otherwise = substring (n+1) sep (w++[c]) cs
+ 
+-- | Split a composed type in its two main parts.
+--
+-- [@input@]: a composed type
+-- [@output@]: the list of the two types composing the composed type
+splitType :: [Char] -> [[Char]]
+splitType [] = []
+splitType t = splitType' 0 [] t
+  where 
+    splitType' :: Int -> [Char] -> [Char] -> [[Char]]
+    splitType' 0 t1 [] = [stripPar t1]
+    splitType' _ t1 [] = ["Type Error"]
+    splitType' n t1 (s:ns)
+      | n == 0 && (elem s ">x") = [(stripPar t1), (stripPar ns)]
+      | s == '(' = splitType' (n+1) (t1 ++ [s]) ns
+      | s == ')' = splitType' (n-1) (t1 ++ [s]) ns
+      | otherwise = splitType' n (t1 ++ [s]) ns
+               
+-- | strip a string from its outer parenthesis if there is any
+stripPar :: [Char] -> [Char]
+stripPar (c:cs)
+  | c == '(' = take ((length cs) - 1) cs
+  | otherwise = c:cs
