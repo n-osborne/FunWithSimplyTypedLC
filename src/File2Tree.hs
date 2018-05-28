a8da5acf551a3f3f4debb2caf4235a3c5c1a42a5
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-05-28 19:23:25 +0200

    add file2tree prog
    Main module (File2Tree.hs)
    update lTyper.cabal
    add an example

---
+++ src/File2Tree.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import System.Environment
+import System.IO
+import Ast2Tree
+import Lambda2Ast
+
+-- | Write in a file which name is given in first argument the tree description
+-- corresponding to the lambda expression described in the file given in second
+-- argument.
+main = do
+  args <- getArgs
+  if (length args) /= 2
+    then putStrLn "Error"
+    else do file <- readFile (head (drop 1 args))
+            writeFile (head args) (file2tree file)
+            where
+              file2tree :: [Char] -> [Char]
+              file2tree file = ast2tree (file2ast file) 
